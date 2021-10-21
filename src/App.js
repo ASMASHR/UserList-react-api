70e44fd69d7d3d7de87a3b0662f4154f01bee531
Asma Sahraoui <asmasahraoui25@hotmail.fr>
2021-10-22 00:11:50 +0100

    first add

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React,{useEffect, useState} from 'react'
+import axios from 'axios';
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
+import './App.css';
+import UserList from './components/UserList';
+import UserInfo from './components/UserInfo'
+function App(props) {
+      const [Users, setUsers]=useState([])
+    useEffect(()=>{
+       axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res=>{console.log(res)
+                    setUsers(res.data)
+                    
+                })
+        .catch(err=>{console.log(err)})
+    },[])
+    
+  console.log(Users)
+  return (
+    <Router>
+    <div className="App">
+      <Switch>
+        <Route exact path="/" render={(props)=><><UserList {...props} Users={Users}/></>}>
+        </Route>
+        <Route path="/userInfo/:id" render={(props)=><><UserInfo {...props} Users={Users}/></>} > 
+        </Route>
+      </Switch>
+    </div>
+   
+    </Router>
+    
+  );
+}
+
+export default App;
